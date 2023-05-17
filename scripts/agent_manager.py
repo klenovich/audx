19dc5797427d4509a0fce802f3b3a4dc81dae2c1
klucas12345 <89214539+klucas12345@users.noreply.github.com>
2023-05-17 00:14:29 -0500

    td/2

--- scripts/agent_manager.py
+++ scripts/agent_manager.py
@@ -3,27 +3,25 @@ from llm_utils import create_chat_completion
 next_key = 0
 agents = {}  # key, (task, full_message_history, model)
 
-# Create new GPT agent
-# TODO: Centralise use of create_chat_completion() to globally enforce token limit
+
+def interact_with_agent(model, messages):
+    # Interact with the model while enforcing the token limit
+    agent_reply = create_chat_completion(
+        model=model,
+        messages=messages
+    )
+    return agent_reply
+
 
 def create_agent(task, prompt, model):
     global next_key
     global agents
 
     messages = [{"role": "user", "content": prompt}, ]
-
-    # Start GTP3 instance
-    agent_reply = create_chat_completion(
-        model=model,
-        messages=messages,
-    )
-
-    # Update full message history
+    agent_reply = interact_with_agent(model, messages)
     messages.append({"role": "assistant", "content": agent_reply})
 
     key = next_key
-    # This is done instead of len(agents) to make keys unique even if agents
-    # are deleted
     next_key += 1
 
     agents[key] = (task, messages, model)
@@ -35,17 +33,8 @@ def message_agent(key, message):
     global agents
 
     task, messages, model = agents[int(key)]
-
-    # Add user message to message history before sending to agent
     messages.append({"role": "user", "content": message})
-
-    # Start GTP3 instance
-    agent_reply = create_chat_completion(
-        model=model,
-        messages=messages,
-    )
-
-    # Update full message history
+    agent_reply = interact_with_agent(model, messages)
     messages.append({"role": "assistant", "content": agent_reply})
 
     return agent_reply
@@ -53,8 +42,6 @@ def message_agent(key, message):
 
 def list_agents():
     global agents
-
-    # Return a list of agent keys and their tasks
     return [(key, task) for key, (task, _, _) in agents.items()]
 
 
